01f09a682014b93713425a4455018aaaaa545262
CrazyCoder <36601305@qq.com>
2020-12-24 15:43:35 +0800

    add function_tools.php

---
+++ function_tools.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Short description.
+ * 
+ * @author  CrazyCoder 
+ * @version 1.0
+ * @package main
+ */
+
+
+/**
+ * Short description.
+ * @param   type    $varname    description
+ * @return  type    description
+ * @access  public or private
+ * @static  makes the class property accessible without needing an instantiation of the class
+ */
+function env()
+{
+    phpinfo();
+} // end func
