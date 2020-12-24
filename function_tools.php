ad3c395c225a9e97dbbfda0f4693aa655a1dbe2b
CrazyCoder <36601305@qq.com>
2020-12-24 16:18:56 +0800

    udpate function_tools.php

--- function_tools.php
+++ function_tools.php
@@ -10,12 +10,21 @@
 
 /**
  * Short description.
+ * @package     main
+ * @subpackage  classes
+ * @abstract    Classes defined as abstract may not be instantiated
+ */
+class function_tools
+{
+    /**
+ * Short description.
  * @param   type    $varname    description
  * @return  type    description
  * @access  public or private
  * @static  makes the class property accessible without needing an instantiation of the class
  */
-function env()
-{
-    phpinfo();
-} // end func
+	public static function env()
+	{
+		phpinfo();
+	} // end func
+} // end class
